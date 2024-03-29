427c9e52babcef43a25a452d7a0eb9876ef4327e
Raúl Cañero Sequera <124920909+SirCaser@users.noreply.github.com>
2024-03-29 16:41:59 +0100

    Las migraciones y los modelos han sido creados.

--- app/Models/Conexiones.php
+++ app/Models/Conexiones.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class tarjeta extends Model
+class Conexiones extends Model
 {
     use HasFactory;
     protected $table = 'conexiones';
--- database/migrations/2014_10_12_000000_juego.php
+++ database/migrations/2014_10_12_000000_juego.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('juego', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre')->unique();
+            $table->string('nombre');
             $table->string('rangos');
         });
     }
