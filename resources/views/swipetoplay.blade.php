d795129c94c010e5b62fa7ba95caa88d28f1f1ef
King0f <nicogm03@gmail.com>
2024-05-03 15:12:39 +0200

    me quiero mataaar

--- resources/views/swipetoplay.blade.php
+++ resources/views/swipetoplay.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>SwipeToPlay</title>
         @viteReactRefresh
-        @vite('resources/js/app.js')
+        @vite('resources/js/swipetoplay/main.jsx')
     </head>
     <body >
         <div id='root'></div>
