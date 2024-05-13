5dee291974fdb814b9da0434e7f52f469309d99b
Ugart3e <pepe.ugarte.galvez@gmail.com>
2024-05-13 19:56:52 +0200

    pagina swipe

--- resources/js/swipetoplay/pages/Swipe.jsx
+++ resources/js/swipetoplay/pages/Swipe.jsx
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import Header from '../components/Header'
 import Filters from '../components/Filters'
+import flechaIzq from '../components/f1.png'
+import flechaDer from '../components/f2.png'
 import { usuarioStore } from '../store/userStore/usuarioStore'
 import imagenUser from '../../../assets/profile.jpg'
 import Icon_social_like_l from '../components/Icon_social_like_l'
 import Icon_actions_close_l from '../components/Icon_actions_close_l'
 import Icon_social_like_m from '../components/Icon_social_like_m'
+import Icon_social_pleasures_xl from '../components/Icon_social_pleasures_xl'
 const Swipe = () => {
   const {usuario, usuarioSwipe, obtenerUsuarioSwipe, obtenerConexionLOL, obtenerConexionValorant, actionSwipe} = usuarioStore((state) => ({
     usuario: state.usuario,
@@ -32,6 +35,7 @@ const Swipe = () => {
 
     fetchData();
   }, []);
+
   const handleAction = async (action) => {
     try{
       actionSwipe(usuarioSwipe.id, action)
@@ -39,8 +43,7 @@ const Swipe = () => {
     }catch{
 
     }
-  }
-
+  };
 
   return (
     <>
@@ -49,33 +52,32 @@ const Swipe = () => {
     <Filters/>
     <div className='flex mx-auto mb-20 mt-14'>
     <div className='flex flex-col justify-around'>
-    <p className='text-center text-4xl font-Swipe font-semibold'>Swipe</p>
+    <div className='flex justify-center'><img src={flechaIzq} className='w-10 h-10 self-center mx-2'/><p className='text-center text-4xl font-Swipe font-semibold text-red-600'>Swipe</p><img src={flechaDer} className='w-10 h-10 self-center mx-2'/></div>
     <div className='flex'>
-    <div className='m-auto mr-5'><button onClick={() => handleAction(2)}> <Icon_actions_close_l fill="red"/></button></div>
-    <div className='w-96 h-96 border-2 bg-gray-300'>
-      <img src={usuarioSwipe.imagen  || imagenUser} className="profile-img m-auto mt-2"/>
-      <p className='text-center font-Swipe font-semibold text-red-600'>{usuarioSwipe.username}</p>
-      <div className='flex font-Swipe justify-center'><p>{usuarioSwipe.likes}</p><Icon_social_like_m fill="red"/></div>
-      <hr />
+    <div className=' self-center mr-5 rounded-full w-12 h-12 hover:bg-red-300 '><div className='ml-2 mt-2'><button onClick={() => handleAction(2)} className=''><Icon_actions_close_l fill="red"/></button></div></div>
+    <div className='w-[400px] h-[450px] border-4 border-red-800 bg-red-400'>
+      <img src={usuarioSwipe.imagen  || imagenUser} className="w-24 rounded-full border-4 border-black m-auto mt-2"/>
+      <p className='text-center text-size-xl font-Swipe font-semibold text-black mt-2'>{usuarioSwipe.username}</p>
+      <div className='flex font-Swipe justify-center mb-4 font-semibold mt-1'><p>{usuarioSwipe.likes}</p><Icon_social_like_m fill="green"/></div>
       <div className='flex justify-around'>
-      <div className='flex flex-col border-r-2'>
-      <h3><b>League of Legends</b></h3>
-      <p>ID: {conexionLOL.riotID}</p>
-      <p>Rank: {conexionLOL.rango} </p>
-      <p>Rol: {conexionLOL.posicion}</p>
+      <div className='flex flex-col'>
+      <h3 className='text-center font-swipe text-size-m font-medium'><b>LOL</b></h3>
+      <p><b>Id:</b> {conexionLOL.riotID}</p>
+      <p><b>Rank:</b> {conexionLOL.rango} </p>
+      <p><b>Rol: </b>{conexionLOL.posicion}</p>
       </div>
       <div className='flex flex-col'>
-      <h3><b>Valorant</b></h3>
-      <p>ID: {conexionValorant.riotID} </p>
-      <p>Rank: {conexionValorant.rango} </p>
-      <p>Rol: {conexionValorant.posicion} </p>
+      <h3 className='text-center font-swipe text-size-m font-medium'><b>VALORANT</b></h3>
+      <p><b>Id:</b> {conexionValorant.riotID}</p>
+      <p><b>Rank:</b> {conexionValorant.rango}</p>
+      <p><b>Rol: </b> {conexionValorant.posicion} </p>
       </div>
       </div>
-      <p className='text-center'>Total de deslizamientos restantes: {usuario.desplazamientos}</p>
+      <p className='text-center mt-10'>Total de deslizamientos restantes: {usuario.desplazamientos}</p>
     </div>
-    <div className='m-auto ml-5'><button onClick={() => handleAction(1)}> <Icon_social_like_l fill="lightgreen"/></button></div>
+    <div className=' self-center ml-5 rounded-full w-12 h-12 hover:bg-green-300 '><div className='ml-2 mt-2'><button onClick={() => handleAction(1)} className=''><Icon_social_like_l fill="green"/></button></div></div>
     </div>
-    <div className='h-16 bg-gray-500 w-1/2 mx-auto mt-4'>Superlike</div>
+    <button><div className=' h-16 bg-yellow-300 w-1/2 mx-auto mt-4 border-2 border-yellow-950 flex justify-center hover:bg-yellow-200 hover:text-red-800 '><p className='self-center font-swipe text-center font-semibold text-size-2xl'>SUPERLIKE</p><div className='self-center ml-4'><Icon_social_pleasures_xl fill='brown'/></div></div></button>
     </div>
     </div>
     </div>
