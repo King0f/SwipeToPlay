3599087e306271354fb231d0eb0922750facd7aa
Nicogm03 <nicogm03@gmail.com>
2024-05-15 12:49:09 +0200

    Remove .env and apiStore.js from tracking

--- resources/js/swipetoplay/store/apiStore/apiStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-export const apiStore = create((set) => ({
-    //RUTA PARA LOCALHOST: "http://localhost/SwipeToPlay/public" 
-    //RUTA PARA PLESK: ""
-    localhost: "http://localhost/SwipeToPlay/public",
-    //RUTA PARA LOCALHOST: "/SwipeToPlay/public/swipetoplay" 
-    //RUTA PARA PLESK: "/swipetoplay"
-    basename: "/SwipeToPlay/public/swipetoplay"
-}))
