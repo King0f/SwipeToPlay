a28d1dbd599b0cbeb4360fa89f540823062f0291
Nicogm03 <nicogm03@gmail.com>
2024-05-15 11:11:14 +0200

    sdfsdf

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('swipetoplay');
-});
+})->name('/');
 
 /* Route::get('/swipetoplay', function () {
     return view('swipetoplay');
