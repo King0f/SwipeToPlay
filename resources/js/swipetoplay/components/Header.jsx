296119107a131e0d30c6c859c01b0045276274d8
Ugart3e <pepe.ugarte.galvez@gmail.com>
2024-05-29 18:22:04 +0200

    header corrección pagina seleccionada

--- resources/js/swipetoplay/components/Header.jsx
+++ resources/js/swipetoplay/components/Header.jsx
@@ -1,112 +1,106 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import imagenEjemplo from '../../../assets/textoLogo.png';
 import '../styles/App.css'
 import ProfileComp from './ProfileComp';
 import { apiStore } from "../store/apiStore/apiStore";
-import {ToastContainer, Zoom, toast } from 'react-toastify'
+import { ToastContainer, Zoom, toast } from 'react-toastify';
 
 export default function Header() {
   const token = !!localStorage.getItem('token');
   const path = apiStore.getState().basename;
-  const [currentPage, setCurrentPage] = useState("Home");
   const navigate = useNavigate();
+  const location = useLocation();  // Obtener la ruta actual
   const popUpNotLogged = () => {
     toast.warning("Necesitas tener una cuenta y estar logeado para entrar a esta página.",
-    {position: 'top-left',theme:'light',transition:Zoom, autoClose:3000, })
+      { position: 'top-left', theme: 'light', transition: Zoom, autoClose: 3000, })
   };
 
   const init = () => {
     navigate(path);
   };
 
+  const isActive = (pagePath) => location.pathname === pagePath ? "font-bold" : "";
+
   return (
     <div className="w-full flex justify-center h-16 bg-black-300 bg-opacity-75">
       <header className="fixed top-0 w-full h-20 clearNav z-50 flex items-center justify-around bg-opacity-35">
         <div className="mx-5 w-1/3">
-          <img src={imagenEjemplo} alt="TextoLogo" onClick={init}/>
+          <img src={imagenEjemplo} alt="TextoLogo" onClick={init} />
         </div>
-    {token ? (
-      <>
-        <div className="">
-            <div
-              onClick={() => setCurrentPage("Home")}
-              className={`text-black font-Swipe ${currentPage === "Home" ? "font-bold" : ""} nav-link`}
-            >
-              <Link to={path}>Inicio</Link>
+        {token ? (
+          <>
+            <div className="">
+              <div
+                onClick={() => navigate(path)}
+                className={`text-black font-Swipe ${isActive(path)} nav-link`}
+              >
+                <Link to={path}>Inicio</Link>
+              </div>
             </div>
-          </div>
 
-          <div className="">
-            <div
-              onClick={() => setCurrentPage("Profile")}
-              className={`text-black font-Swipe ${currentPage === "Profile" ? "font-bold" : ""} nav-link`}
-            >
-            <Link to={path + "/Swipe"}>Start Swiping</Link>
-
+            <div className="">
+              <div
+                onClick={() => navigate(path + "/Swipe")}
+                className={`text-black font-Swipe ${isActive(path + "/Swipe")} nav-link`}
+              >
+                <Link to={path + "/Swipe"}>Start Swiping</Link>
+              </div>
             </div>
-          </div>
 
-          <div className="">
-            <div
-              onClick={() => setCurrentPage("Games")}
-              className={`text-black font-Swipe ${currentPage === "Games" ? "font-bold" : ""} nav-link`}
-            >
-              <Link to={path + "/games"}>Games</Link>
+            <div className="">
+              <div
+                onClick={() => navigate(path + "/games")}
+                className={`text-black font-Swipe ${isActive(path + "/games")} nav-link`}
+              >
+                <Link to={path + "/games"}>Games</Link>
+              </div>
             </div>
-          </div>
 
-          <div className="">
-            <div
-              onClick={() => setCurrentPage("Chatting")}
-              className={`text-black font-Swipe ${currentPage === "Chatting" ? "font-bold" : ""} nav-link`}
-            >
-              <Link to={path + "/Chat"}>Start Chatting</Link>
+            <div className="">
+              <div
+                onClick={() => navigate(path + "/Chat")}
+                className={`text-black font-Swipe ${isActive(path + "/Chat")} nav-link`}
+              >
+                <Link to={path + "/Chat"}>Start Chatting</Link>
+              </div>
             </div>
-          </div>
-      </>
-    )
-    : (
-      <>
-      <ToastContainer pauseOnFocusLoss={false} limit={3} />
-        <div className="">
-            <div
-              onClick={() => setCurrentPage("Home")}
-              className={`text-black font-Swipe ${currentPage === "Home" ? "font-bold" : ""} nav-link`}
-            >
-              <Link to={path}>Inicio</Link>
-            </div>
-          </div>
+          </>
+        )
+          : (
+            <>
+              <ToastContainer pauseOnFocusLoss={false} limit={3} />
+              <div className="">
+                <div
+                  onClick={() => navigate(path)}
+                  className={`text-black font-Swipe ${isActive(path)} nav-link`}
+                >
+                  <Link to={path}>Inicio</Link>
+                </div>
+              </div>
 
-          <div className="">
-            <div
-              className={`text-black font-Swipe ${currentPage === "Profile" ? "font-bold" : ""} nav-link`}
-            >
-            <Link><button onClick={popUpNotLogged}>Start Swiping</button></Link>
+              <div className="">
+                <div className={`text-black font-Swipe nav-link`}>
+                  <Link><button onClick={popUpNotLogged}>Start Swiping</button></Link>
+                </div>
+              </div>
 
-            </div>
-          </div>
+              <div className="">
+                <div className={`text-black font-Swipe nav-link`}>
+                  <Link><button onClick={popUpNotLogged}>Games</button></Link>
+                </div>
+              </div>
 
-          <div className="">
-            <div
-              className={`text-black font-Swipe ${currentPage === "Games" ? "font-bold" : ""} nav-link`}
-            >
-              <Link><button onClick={popUpNotLogged}>Games</button></Link>
-            </div>
-          </div>
+              <div className="">
+                <div className={`text-black font-Swipe nav-link`}>
+                  <Link><button onClick={popUpNotLogged}>Start Chatting</button></Link>
+                </div>
+              </div>
+            </>
+          )
+        }
 
-          <div className="">
-            <div
-              className={`text-black font-Swipe ${currentPage === "Chatting" ? "font-bold" : ""} nav-link`}
-            >
-              <Link><button onClick={popUpNotLogged}>Start Chatting</button></Link>
-            </div>
-          </div>
-      </>
-    )
-    }
-
-      <ProfileComp/>
+        <ProfileComp />
       </header>
     </div>
   );
