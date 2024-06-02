a56a2b96db8591959bf846cd136459698774f4fd
King0f <nicogm03@gmail.com>
2024-06-02 20:36:08 +0200

    Enseñando nombre del usuario en historial de matches.

--- resources/js/swipetoplay/pages/HistorialMatch.jsx
+++ resources/js/swipetoplay/pages/HistorialMatch.jsx
@@ -14,20 +14,39 @@ const HistorialMatch = () => {
     const navigate = useNavigate();
     const path = apiStore.getState().basename;
     const [selectedMatch, setSelectedMatch] = useState()
+    const [users, setUsers] = useState({});
     const [showPopup, setShowPopup] = useState(false);
     const { matches, getMatches, delMatch } = matchStore((state) => ({
         matches: state.matches,
         getMatches: state.getMatches,
         delMatch: state.delMatch,
     }));
-    const { usuario,borrarUsuario } = usuarioStore((state) => ({
+    const { usuario,obtenerUsuarioById} = usuarioStore((state) => ({
         usuario: state.usuario,
-        borrarUsuario: state.borrarUsuario
+        obtenerUsuarioById: state.obtenerUsuarioById
     }));
 
     useEffect(() => {
         getMatches();
     }, []);
+    useEffect(() => {
+        fetchUsers();
+    }, [matches]);
+    
+    const fetchUsers = async () => {
+        const usersData = {};
+        for (const match of matches) {
+          const displayUserId = match.id_user2 === usuario.id ? match.id_user1 : match.id_user2;
+          if (!usersData[displayUserId]) {
+            const user = await obtenerUsuarioById(displayUserId);
+            if (user) {
+              usersData[displayUserId] = user;
+            }
+          }
+        }
+        setUsers(usersData);
+      };
+  
 
     const getGameName = (gameId) => {
         const gameNames = {
@@ -66,14 +85,15 @@ const HistorialMatch = () => {
                             </thead>
                             <tbody className="bg-white divide-y divide-gray-200">
                                 {matches.map((match) => {
-                                    const displayUserId = match.id_user2 === usuario.id ? match.id_user1 : match.id_user2;
+                                     const displayUserId = match.id_user2 === usuario.id ? match.id_user1 : match.id_user2;
+                                     const user = users[displayUserId];
                                     return (
                                         <tr key={match.id}>
                                             <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                                             {match.created_at ? format(parseISO(match.created_at), 'dd/MM/yyyy') : ''}
                                             </td>
                                             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                                {displayUserId}
+                                            {user ? user.username : 'Cargando...'}
                                             </td>
                                             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                             {getGameName(match.id_juego)}
@@ -89,6 +109,27 @@ const HistorialMatch = () => {
                     </div>
                 </div>
             </div>
+            {showPopup && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
+          <div className="bg-white p-5 rounded shadow-lg text-center">
+            <p>¿Deseas eliminar este match?</p>
+            <div className="mt-4 flex justify-center space-x-4">
+              <button
+                onClick={handleDelete}
+                className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-red-500"
+              >
+                Sí
+              </button>
+              <button
+                onClick={handleCancel}
+                className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-red-500"
+              >
+                No
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
             <Footer />
         </div>
     );
