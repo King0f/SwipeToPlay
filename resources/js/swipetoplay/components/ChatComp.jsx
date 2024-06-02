966b6e1379f725078f4dc33bb7406b2c5ffac080
King0f <nicogm03@gmail.com>
2024-06-02 19:42:58 +0200

    Probando que el chat funciona bien en plesk.

--- resources/js/swipetoplay/components/ChatComp.jsx
+++ resources/js/swipetoplay/components/ChatComp.jsx
@@ -25,7 +25,7 @@ const ChatComp = ({ chatId, urlReceived}) => {
         fetchMessages();  // Carga inicial de mensajes
 
         // Configurar polling
-        let intervalId = setInterval(fetchMessages, 100000); // Polling cada 5 segundos
+        let intervalId = setInterval(fetchMessages, 5000); // Polling cada 5 segundos
 
         // Ajustar el polling según la visibilidad de la pestaña
         const handleVisibilityChange = () => {
@@ -33,7 +33,7 @@ const ChatComp = ({ chatId, urlReceived}) => {
                 clearInterval(intervalId); // Detener polling cuando la pestaña no es visible
             } else {
                 fetchMessages(); // Cargar mensajes al volver a la pestaña
-                intervalId = setInterval(fetchMessages, 100000); // Reanudar polling
+                intervalId = setInterval(fetchMessages, 5000); // Reanudar polling
             }
         };
         document.addEventListener('visibilitychange', handleVisibilityChange);
